df4829bb03fe080948abd690b96b10c5dc05ac6c
Mohit-9440 <mohitsharma8864@gmail.com>
2025-04-26 22:43:14 +0530

    SignUp

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,36 @@ import User from "../models/User.js";
 import { generateToken } from "../config/jwt.js";
 import bcrypt from "bcryptjs";
 
+export const signupUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password)
+    return res.status(400).json({ error: "All fields required" });
+
+  const existing = await User.findOne({ email });
+  if (existing)
+    return res.status(400).json({ error: "Email already registered" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    role: "employee",
+  });
+
+  const token = generateToken(newUser._id);
+  res.status(201).json({
+    token,
+    user: {
+      id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    },
+  });
+};
+
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
