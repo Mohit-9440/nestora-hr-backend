3b0d5569f7e32c058073c4f2c4888f517d0c26a6
Mohit-9440 <mohitsharma8864@gmail.com>
2025-04-26 19:40:43 +0530

    jwt

--- config/jwt.js
+++ config/jwt.js
@@ -1,21 +1,7 @@
-import User from "../models/User.js";
-import { generateToken } from "../config/jwt.js";
-import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-
-  res.json({
-    id: user._id,
-    name: user.name,
-    email: user.email,
-    role: user.role,
-    token: generateToken(user._id),
+export const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
   });
 };
