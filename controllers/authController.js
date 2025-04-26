70aa7e3ed499d7331c8473c0ce8389a0e57b143e
Mohit-9440 <mohitsharma8864@gmail.com>
2025-04-26 19:44:44 +0530

    Bcrypt in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,21 @@
-const User = require('../models/User');
+import User from "../models/User.js";
+import { generateToken } from "../config/jwt.js";
+import bcrypt from "bcryptjs";
 
-exports.login = async (req, res) => {
+export const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email, password });
-    if (!user) return res.status(400).json({ message: 'Invalid Credentials' });
 
-    res.json({
-      id: user._id,
-      name: user.name,
-      role: user.role,
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+  res.json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id),
+  });
 };
